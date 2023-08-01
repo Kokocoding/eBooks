d36bb50a80b5f032b9c2524826460905e0c5cb0f
stytle704726 <stytle704726@gmail.com>
2023-08-01 17:24:30 +0800

    no message

---
+++ eBooks/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Input;
+
+namespace eBooks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] imageFiles = { "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" };
+        private int currentImageIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetImageSource(imageFiles[currentImageIndex]);
+        }
+
+        private void SetImageSource(string imagePath)
+        {
+            string destinationPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", imagePath);
+            eBook.Source = new BitmapImage(new Uri(destinationPath));
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentImageIndex = (currentImageIndex + 1) % imageFiles.Length;
+            SetImageSource(imageFiles[currentImageIndex]);
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentImageIndex = (currentImageIndex - 1 + imageFiles.Length) % imageFiles.Length;
+            if (currentImageIndex < 0) return;
+            SetImageSource(imageFiles[currentImageIndex]);
+        }
+    }
+}
